2024cb15e31acbe912dc71efb8bfe77687e20e7c
mahmoud11mg <mahmoud11magd@gmail.com>
2024-08-02 19:01:50 +0200

    edit DB

--- src/app/admin/user-admin/AdminUserTable.tsx
+++ src/app/admin/user-admin/AdminUserTable.tsx
@@ -7,8 +7,6 @@ import { User } from "@/utils/types"; // Update import path if needed
 import AdminUsersTableClient from "./AdminUsersTableClient";
 import { NextRequest } from "next/server";
 
-
-
 const AdminUsersTable = async () => {
     const token = cookies().get("jwtToken")?.value || "";
     if (!token) redirect("/");
@@ -18,28 +16,21 @@ const AdminUsersTable = async () => {
 
     const users: User[] = await getAllUsers();
     const count: number = await prisma.user.count();
- async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+    async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
         try {
+            const body = await request.json();
 
-             const body = await request.json();
-          
-    
             // Other server logic...
-    
         } catch (error: any) {
-     
             // Handle error...
         }
     }
-    
 
     return (
         <section className="p-5">
             <h1 className="mb-7 text-2xl font-semibold text-gray-700">Users</h1>
             <AdminUsersTableClient users={users} />
-           
         </section>
     );
 };
-
 export default AdminUsersTable;
